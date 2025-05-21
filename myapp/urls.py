1141634dea049f619b97c05b2cfc6fa66dae0077
tomqle <tomqle1@gmail.com>
2025-05-20 22:58:46 -0500

    Add error 404 page

--- myapp/urls.py
+++ myapp/urls.py
@@ -17,9 +17,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import RedirectView
 
-from lookup.views import HomeView, KeyView, RemoteHomeView, RemoteView, ShellHomeView, ShellView, KeyShellHomeView, KeyShellView, RemoteShellHomeView, RemoteShellView, EmergencyKeyHomeView, EmergencyKeyView
+from lookup.views import HomeView, KeyView, RemoteHomeView, RemoteView, KeyShellHomeView, KeyShellView, RemoteShellHomeView, RemoteShellView, EmergencyKeyHomeView, EmergencyKeyView
 
 admin.site.site_header = "Keys Lookup Admin"
 admin.site.site_title = "Keys Lookup Portal"
@@ -31,15 +30,12 @@ urlpatterns = [
     path('keys/<int:id>/', KeyView.as_view(), name='key'),
     path('remotes/', RemoteHomeView.as_view(), name='remote_home'),
     path('remotes/<int:id>/', RemoteView.as_view(), name='remote'),
-    #path('shells/', ShellHomeView.as_view(), name='shell_home'),
-    #path('shells/<int:id>/', ShellView.as_view(), name='shell'),
     path('key_shells/', KeyShellHomeView.as_view(), name='key_shell_home'),
     path('key_shells/<int:id>/', KeyShellView.as_view(), name='key_shell'),
     path('remote_shells/', RemoteShellHomeView.as_view(), name='remote_shell_home'),
     path('remote_shells/<int:id>/', RemoteShellView.as_view(), name='remote_shell'),
     path('emergency_keys/', EmergencyKeyHomeView.as_view(), name='emergency_key_home'),
     path('emergency_keys/<int:id>/', EmergencyKeyView.as_view(), name='emergency_key'),
-    path('/', RedirectView.as_view(url='keys/', permanent=False), name='index'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
